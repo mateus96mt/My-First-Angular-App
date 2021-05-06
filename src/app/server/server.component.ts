cd24bd5be0b5919116e2d09cf0743183717ac247
Mateus Teixeira Magalhães <mateus96mt@gmail.com>
2021-05-06 15:15:49 -0300

    Dynamicly styling

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,7 +8,23 @@ export class ServerComponent{
   serverId = 10;
   serverStatus = ' offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus(){
     return this.serverStatus;
   }
+
+  getColor(){
+    switch (this.serverStatus){
+      case 'online':
+        return 'green';
+      case 'offline':
+        return 'red';
+      default:
+        return 'black';
+    }
+  }
+
 }
